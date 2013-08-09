b6ec3ab29237419c3180f87a8b8c8bfacf1d1063
Zain Memon <zain@inzain.net>
2013-08-08 18:51:17 -0700

    Update models.py

--- django_passbook/models.py
+++ django_passbook/models.py
@@ -5,9 +5,9 @@ class Pass(models.Model):
     """
     Pass instance
     """
-    pass_type_identifier = models.CharField(max_length=50)
-    serial_number = models.CharField(max_length=50)
-    authentication_token = models.CharField(max_length=50)
+    pass_type_identifier = models.CharField(max_length=254)
+    serial_number = models.CharField(max_length=254)
+    authentication_token = models.CharField(max_length=254)
     data = models.FileField(upload_to='passes')
     updated_at = models.DateTimeField()
 
@@ -22,8 +22,8 @@ class Registration(models.Model):
     """
     Registration of a Pass on a device
     """
-    device_library_identifier = models.CharField(max_length=50)
-    push_token = models.CharField(max_length=50)
+    device_library_identifier = models.CharField(max_length=254)
+    push_token = models.CharField(max_length=254)
     pazz = models.ForeignKey(Pass)
 
     def __unicode__(self):
